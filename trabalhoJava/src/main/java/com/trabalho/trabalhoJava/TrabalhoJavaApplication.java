aa9e91b3ebb8789d7498074e22f651034b2c370c
marcos vinícius <marcosmantoani90@gmail.com>
2025-10-13 22:15:49 -0300

    adding new project and updating some files

---
+++ trabalhoJava/src/main/java/com/trabalho/trabalhoJava/TrabalhoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.trabalhoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoJavaApplication.class, args);
+	}
+
+}
---
+++ trabalhoJava/src/test/java/com/trabalho/trabalhoJava/TrabalhoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.trabalhoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
